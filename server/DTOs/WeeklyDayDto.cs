a1d3ab8c4cdd3bd2db7fa5fb8db2042160ed1f19
Abanoub Milad <abanoubmilad64@gmail.com>
2025-12-11 21:46:11 +0200

    Add files via upload

---
+++ server/DTOs/WeeklyDayDto.cs
@@ -0,0 +1,5 @@
+﻿namespace backend.DTOs
+{
+    // WeeklyDayDto file kept for historical reasons. Definitions moved to DailyScheduleDto and WeeklyScheduleDto.
+    public static class WeeklyDayDtoPlaceholder { }
+}
